bc45f9cf4132c19141734fb7d5941fcd9a8c2a90
Shaurya Suyal <133216762+shauryasuyal@users.noreply.github.com>
2025-10-31 22:37:58 +0530

    Changes
    using .unique to get unique values automatically and creating a dynamic order dictionary

--- Ordinal Encoding.py
+++ Ordinal Encoding.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
-data = { 'Size': ['Small', 'Medium', 'Large', 'Medium', 'Small'],
-    'Type': ['Caramel', 'Salted', 'Cheese', 'Salted', 'Caramel']
-}
-df = pd.DataFrame(data)
-order = {'Small': 1, 'Medium': 2, 'Large': 3}
+data = { 'Size': ['Small', 'Medium', 'Large', 'Medium', 'Small'],}
 
+
+df = pd.DataFrame(data)
+unique_values = list(df['Size'].unique())
+order = {val: i+1 for i, val in enumerate(unique_values)}
 encoded = []
 for i in range(len(df)):
-    size = df.loc[i, 'Size']
-    if size in order:
-     encoded.append(order[size])
+    val = df.loc[i, 'Size']
+    encoded.append(order[val])
 
-df['Size Encoded'] = encoded
+
+df['Size_Encoded'] = encoded
 print("\n After encoding - ")
 print(df)
